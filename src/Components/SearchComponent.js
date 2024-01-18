ac4170e16e1669fc34a5e5300efd694d4009f131
omar-yasser-ayoub <omar.ayoub@student.guc.edu.eg>
2024-01-18 16:07:20 +0200

    add search bar

--- src/Components/SearchComponent.js
+++ src/Components/SearchComponent.js
@@ -1,10 +1,21 @@
-import ArtistComponent from "./AlbumComponent";
-import CardComponent from "./CardComponent";
+import VideoComponent from "./VideoComponent";
+import { ReactComponent as SearchSVG } from '../Assets/Search.svg';
 function SearchComponent(props) {
     return (
-      <div className="z-0 w-screen absolute bg-gray-800 h-screen bg-fixed">
+      <div className="z-0 w-screen absolute bg-gray-800 h-screen bg-fixed  font-semibold text-white">
         <div className="mx-4">
-          <h1 className="text-4xl font-semibold text-white mt-8">Search</h1> 
+          <h1 className="text-2xl mt-8">Search</h1> 
+          <div className="inline-flex rounded-md bg-white p-3 mt-4 w-full">
+            <SearchSVG width={20} height={20} className="mx-2 text-black"/>
+            <h1 className="text-sm text-black">What do you want to listen to?</h1>
+          </div>
+          <h1>Explore your genres</h1>
+          <div className="grid grid-cols-3">
+            <VideoComponent/>
+            <VideoComponent/>
+            <VideoComponent/>
+          </div>
+          <h1>Browse all</h1>
         </div>
       </div>
     );
---
+++ src/Components/VideoComponent.js
@@ -0,0 +1,12 @@
+
+function VideoComponent(props) {
+    return (
+      <div className="">
+        <video src="https://v16-webapp-prime.tiktok.com/video/tos/useast2a/tos-useast2a-ve-0068c002/oI1QE6GUDeATLpgQoRXnJXHbZQOO1JkLXBjbBe/?a=1988&ch=0&cr=3&dr=0&lr=unwatermarked&cd=0%7C0%7C0%7C&cv=1&br=954&bt=477&bti=ODszNWYuMDE6&cs=0&ds=2&ft=4fUEKMUj8Zmo0k6vk94jVR31ZpWrKsd.&mime_type=video_mp4&qs=0&rc=OjUzM2g8ODVmOjw5ODk6M0BpMztodjc6Zm9zbDMzNzczM0AtMC4tMTAuNTUxMmJgYzUzYSMyb2BocjRvbmZgLS1kMTZzcw%3D%3D&btag=e00090000&expire=1705608261&l=2024011814021232A7EEBADDC3DD1B3A05&ply_type=2&policy=2&signature=ebbbbb1f8b1700bc94c7095e9fdc5d8b&tk=tt_chain_token">
+        </video>
+      </div>
+    );
+  }
+  
+  export default VideoComponent;
+  
