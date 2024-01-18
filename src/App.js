932f8be926ea135646fb16e5e495582be9fe19e6
omar-yasser-ayoub <omar.ayoub@student.guc.edu.eg>
2024-01-18 13:48:11 +0200

    button toggles and states, animations not working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
 import { ReactComponent as HomeSVG } from './Assets/Home.svg';
-import { ReactComponent as SerachSVG } from './Assets/Search.svg';
+import { ReactComponent as SearchSVG } from './Assets/Search.svg';
 import { ReactComponent as LibrarySVG } from './Assets/Library.svg';
 import { ReactComponent as PlaySVG } from './Assets/Play.svg';
 import { ReactComponent as LikeSVG } from './Assets/Heart.svg';
+import { ReactComponent as HomeSelectedSVG } from './Assets/HomeFilled.svg';
+import { ReactComponent as SearchSelectedSVG } from './Assets/SearchFilled.svg';
+import { ReactComponent as LibrarySelectedSVG } from './Assets/LibraryFilled.svg';
 import CardComponent from './Components/CardComponent';
 import ArtistComponent from './Components/ArtistComponent';
+import { useEffect, useState} from 'react';
+
+
 function App() {
+
+  const [currentMenu, setMenu] = useState("");
+  const [isAnimating, setAnimating] = useState(false);
+
+  const handleClick = () => {
+    setAnimating(true);
+    setTimeout(() => {
+      setAnimating(false);
+    }, 1000);
+  };
+  const handleMenuClick = (Menu) => {
+    setMenu(Menu);
+    handleClick();
+  }
   return (
     <div>
       <div className="z-10 bottom-0 w-screen fixed">
@@ -14,16 +34,16 @@ function App() {
           <div className="bg-gradient-to-t from-black to-transparent absolute bottom-0 w-screen h-20 ">
           </div>
           <div className="bg-gradient-to-t from-black to-transparent absolute bottom-0 w-screen grid grid-cols-3 justify-center h-20 items-center">
-              <div className="text-gray-300 text-sm text-center">
-                <HomeSVG width={25} height={25} className="mx-auto" />
+              <div className="text-gray-300 text-sm text-center" onClick={() => handleMenuClick("Home")}>
+                {currentMenu === "Home" ? (<HomeSelectedSVG width={25} height={25} className= {isAnimating ? "buttonAnimation mx-auto" : "mx-auto"} />) : (<HomeSVG width={25} height={25} className="mx-auto" />)}
                 <h1 className="mt-1">Home</h1>
               </div>
-              <div className="text-gray-300 text-sm text-center">
-                <SerachSVG width={25} height={25} className="mx-auto fill-current" />
+              <div className="text-gray-300 text-sm text-center" onClick={() => handleMenuClick("Search")}>
+                {currentMenu === "Search" ? (<SearchSelectedSVG width={25} height={25} className= {isAnimating ? "buttonAnimation mx-auto" : "mx-auto"} />) : (<SearchSVG width={25} height={25} className="mx-auto" />)}
                 <h1 className="mt-1">Search</h1>
               </div>  
-              <div className="text-gray-300 text-sm text-center">
-                <LibrarySVG width={25} height={25} className="mx-auto" />
+              <div className="text-gray-300 text-sm text-center" onClick={() => handleMenuClick("Library")}>
+                {currentMenu === "Library" ? (<LibrarySelectedSVG width={25} height={25} className= {isAnimating ? "buttonAnimation mx-auto" : "mx-auto"} />) : (<LibrarySVG width={25} height={25} className="mx-auto" />)}
                 <h1 className="mt-1">Your Library</h1>
               </div> 
           </div>
@@ -50,7 +70,7 @@ function App() {
       <div className="z-0 w-screen absolute bg-gray-800 h-max bg-fixed">
         <div className="mx-4">
           <h1 className="text-4xl font-semibold text-white mt-8">Good evening</h1> 
-          <div className='grid grid-cols-2 gap-2 mt-6 justify-center items-center'>
+          <div className='grid grid-cols-2 gap-2 mt-6 justify-center items-center '>
             <CardComponent title="A Call" img="https://i1.sndcdn.com/artworks-qqdzEemx0vyT2Rr9-s2yzZw-t500x500.jpg"/>
             <CardComponent title="bugging!" img="https://images.genius.com/08f7968612f58dfe31462ea1bbd29119.1000x1000x1.png"/>
             <CardComponent title="Optifine" img="https://i1.sndcdn.com/artworks-qtTeFHOJZRXR-0-t500x500.jpg"/>
@@ -62,7 +82,7 @@ function App() {
           </div>
           <h1 className="text-2xl font-semibold text-white mt-6">Jump back in</h1>
           <div className='overflow-y-hidden'>
-            <div className="flex flex-row gap-2 mt-6 ">
+            <div className="flex flex-row gap-2 mt-6">
               <ArtistComponent title="A Call" artist="EDEN" type="Single" img="https://i1.sndcdn.com/artworks-qqdzEemx0vyT2Rr9-s2yzZw-t500x500.jpg"/>
               <ArtistComponent title="bugging!" artist="brakence" type="Single" img="https://images.genius.com/08f7968612f58dfe31462ea1bbd29119.1000x1000x1.png"/>
               <ArtistComponent title="Optifine" artist="gabby start" type="Single" img="https://i1.sndcdn.com/artworks-qtTeFHOJZRXR-0-t500x500.jpg"/>
