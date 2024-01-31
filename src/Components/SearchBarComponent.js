f53948a3127080bfd1e9a031a8e9076df8a64f7d
omar-yasser-ayoub <omar.ayoub@student.guc.edu.eg>
2024-01-31 23:03:11 +0200

    cleaned up search code, made placeholder loading divs

--- src/Components/SearchBarComponent.js
+++ src/Components/SearchBarComponent.js
@@ -7,10 +7,11 @@ function SearchBarComponent(props) {
   const [isSearching, setSearching] = useState(false);
   const [searchitems, setSearchItems] = useState([])
   const [searchKey,setSearchKey] = useState([])
-
+  const [isLoaded, setLoaded] = useState(false)
 
   function handleSearch(e) {
     setSearchKey(e.target.value)
+    setLoaded(false)
     searchArtists();
   }
   const searchArtists = async () => {
@@ -55,6 +56,7 @@ function SearchBarComponent(props) {
         }
         console.log(Array.from(total))
         setSearchItems(Array.from(total));
+        setLoaded(true)
       } catch (error) {
         console.error("Error searching artists:", error);
       }
@@ -77,6 +79,20 @@ function SearchBarComponent(props) {
       </div>
     ))
   }
+  const renderLoadingDiv = () => {
+    const items = []
+    for (let i = 0; i < 15; i++) {
+      items.push(
+        <div key={i} className="items-center grid grid-cols-1 my-2">
+          <div className="inline-flex h-16 items-center justify-start">
+               <div className="h-4/5 aspect-square bg-light-bg animate-pulse" />            
+              <div className="h-2 w-3/4 bg-light-bg ml-2" />
+          </div>
+        </div>
+      );
+    }
+    return items 
+  }
 
   function handleClick() {
     setSearching(!isSearching);
@@ -87,11 +103,11 @@ function SearchBarComponent(props) {
       <div>
         <div className="inline-flex rounded-md bg-white p-3 mt-4 w-full font items-center" onClick={handleClick}>
               <SearchSVG width={20} height={20} className="mx-2 text-black"/>
-              <h1 className="text-sm text-black mt-1">What do you want to listen to?</h1>
+              <h1 className="text-sm text-black">What do you want to listen to?</h1>
         </div>
         <div className= {isSearching ? 'absolute w-screen min-h-full bg-dark-bg left-0 top-0 z-10' : 'hidden' }>
           <div className='bg-search-bar-bg-light h-16 p-4 grid grid-cols-7 w-screen items-center'>
-            <div className='bg-search-bar-bg p-2 col-span-6 text-sm text-text-grey rounded-xl inline-flex'>
+            <div className='bg-search-bar-bg p-2 col-span-6 text-sm text-text-grey rounded-xl inline-flex' >
               <SearchSVG width={20} height={20} className="mr-2 ml-1 text-text-grey"/>
               <input className='bg-search-bar-bg w-full' placeholder="What do you want to listen to?" onChange={handleSearch} ></input>
             </div>
@@ -100,7 +116,7 @@ function SearchBarComponent(props) {
             </div>
           </div>
         <div className='mx-4 mt-4'>
-          {renderTotal()}
+          {!isLoaded && searchKey.length > 1 ? renderLoadingDiv() : renderTotal()}
         </div>  
         </div>
       </div>
