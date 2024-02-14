8c4dd948dea7b40237184072a8d7c35f0ad9feaa
Farah065 <farahahmad065@gmail.com>
2024-02-14 23:11:25 +0200

    added library component for mobile

---
+++ src/Components/LibraryCard.js
@@ -0,0 +1,13 @@
+function LibraryCard(props) {
+    return (
+      <div className="inline-flex font-SpotifyCircular-Medium w-full items-center mb-5">
+        <div className="w-16 h-16 bg-gray-400"></div>
+        <div className="flex flex-col ml-3">
+            <h1 className="text-white">character development</h1>
+            <h2 className="text-text-grey text-sm">Album • reeka</h2>
+        </div>
+      </div>
+    );
+  }
+  
+  export default LibraryCard;
--- src/Components/LibraryComponent.js
+++ src/Components/LibraryComponent.js
@@ -1,8 +1,58 @@
-import ArtistComponent from "./AlbumComponent";
-import CardComponent from "./CardComponent";
+import LibraryCard from "./LibraryCard";
+import { ReactComponent as FilterSVG } from '../Assets/Filter.svg';
+
 function LibraryComponent(props) {
     return (
-      <div className="z-0 w-screen absolute bg-dark-bg h-screen bg-fixed">
+      <div className="z-0 w-full absolute bg-dark-bg h-full bg-fixed text-white font-SpotifyCircular-Medium overflow-y-scroll">
+        <div className="mx-4">
+          <div className="fixed w-full z-10">
+            <div className="bg-dark-bg">
+              <h1 className="text-2xl pt-8">Your Library</h1>
+              <div className="inline-flex gap-2 mt-5 mb-2">
+                <div className="bg-light-bg py-2 px-4 rounded-full">
+                  <h1>Playlists</h1>
+                </div>
+                <div className="bg-light-bg py-2 px-4 rounded-full">
+                  <h1>Albums</h1>
+                </div>
+                <div className="bg-light-bg py-2 px-4 rounded-full">
+                  <h1>Artists</h1>
+                </div>
+              </div>
+            </div>
+            <div className="-mx-4 h-1 bg-gradient-to-t to-black from-transparent"></div>
+          </div>
+          <div className="h-full absolute top-32 pt-4">
+            <div className="mb-3">
+              <div className="inline-flex gap-2 items-center">
+                <FilterSVG className="w-5"/>
+                <h2 className="mt-1">Recents</h2>
+              </div>
+            </div>
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <LibraryCard />
+            <div className="h-36 md:hidden">
+
+          </div>
+          </div>
+        </div>
       </div>
     );
   }
